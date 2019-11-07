53612d2d5d772a4cca4a43326a5fa1b4cfc05a0f
Joao Neto <joaoneto@a5d6a6ee4302>
2019-11-07 15:09:59 -0200

    Updated Files!

--- crud.py
+++ crud.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
-es = Elasticsearch("192.168.1.5")
+es = Elasticsearch("10.0.1.69")
 
 
 ######### Funções
@@ -27,6 +27,18 @@ def elastic_search(search):
 		result_list.append("%s" % (hit["_source"]["nome"]))
 	return result_list
 
+def elastic_full_search(search):
+	
+	doc = {
+	    'nome': search ,
+	}
+
+	nome = str(doc['nome'])
+	res = es.search(index="usuarios", body={"query": {"match_phrase_prefix": { "nome": "%s" % (nome)}}})
+	result_list = []
+	for hit in res['hits']['hits']:
+		result_list.append("Nome: %s|Cargo: %s|Telefone: %s|Email: %s" % (hit["_source"]["nome"],hit["_source"]["cargo"],hit["_source"]["contatos"]["telefone"],hit["_source"]["contatos"]["email"]))
+	return result_list
 #####################################
 
 @app.route('/')
@@ -41,7 +53,9 @@ def search():
 	temp_var = ""
 	if len(search_result) > 0:
 		for i in range(len(search_result)):
-			temp_var = temp_var + "<a href='http://localhost/search?q="+ search_result[i] +"' style='text-decoration:none'>" + search_result[i] + "</a><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
+			temp_var = temp_var + "<button style='background-color: transparent; border: none;' onclick='dosomething(\""+search_result[i]+"\")' >"+search_result[i]+"</button><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
+			#temp_var = temp_var + "<button style='background-color: transparent; border: none;' onclick=\"alert('"+search_result[i]+"')\">"+search_result[i]+"</button><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
+			#temp_var = temp_var + "<a href='http://localhost/search?q="+ search_result[i] +"' style='text-decoration:none'>" + search_result[i] + "</a><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
 			# Substituir A com HREF por uma DIV com Evento OnClick
 			## 1 - Testar OnClick com uma funcao Alert contendo o texto.
 			## 2 - Funcionando o passo 1, substituir a funcao Alert para a selecao de busca.
@@ -52,6 +66,31 @@ def search():
 	
 	return temp_var
 	
+@app.route('/fullsearch',methods= ['GET'])
+def fullsearch():
+	search = request.args.get("qq")
+	print search
+	full_search_result = elastic_full_search(search)
+	return full_search_result[0]
+	
+	#return full_search_result
+	#return "ihuu"
+	# search_result = elastic_search(search)
+	# temp_var = ""
+	# if len(search_result) > 0:
+	# 	for i in range(len(search_result)):
+	# 		temp_var = temp_var + "<button style='background-color: transparent; border: none;' onclick='dosomething(\""+search_result[i]+"\")' >"+search_result[i]+"</button><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
+	# 		#temp_var = temp_var + "<button style='background-color: transparent; border: none;' onclick=\"alert('"+search_result[i]+"')\">"+search_result[i]+"</button><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
+	# 		#temp_var = temp_var + "<a href='http://localhost/search?q="+ search_result[i] +"' style='text-decoration:none'>" + search_result[i] + "</a><p style='margin-block-start: 4px; margin-block-end: 4px;'>"
+	# 		# Substituir A com HREF por uma DIV com Evento OnClick
+	# 		## 1 - Testar OnClick com uma funcao Alert contendo o texto.
+	# 		## 2 - Funcionando o passo 1, substituir a funcao Alert para a selecao de busca.
+	# 		# Passar LI
+	# 		# Validar a possibilidade de passar Json com os dados ao inves de enviar a TAG HTML completa.
+	# else:
+	# 	temp_var = "nenhum valor encontrado! Tente a letra <b>J</b>" 
+	
+	# return temp_var
 	
 if __name__ == "__main__":
 	app.run("0.0.0.0", "80", debug=True)
